297f1a650d0620ccb345cfedba345443fe7a2c20
CarlosOjeda25 <duvan.duvanojeda@gmail.com>
2025-08-26 22:08:42 -0500

    Replace hello_world route with multiple opc routes returning localized messages

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World Carlos Ojeda Moreno!'
+@app.route('/opc0')
+def home1():
+    return 'Hola, esta es la opcion 0'
+
+@app.route('/opc1')
+def home2():
+    return 'Hola, esta es la opcion 1'
+
+@app.route('/opc2')
+def home3():
+    return 'Hola, esta es la opcion 2'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5550)
