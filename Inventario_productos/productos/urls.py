4ca3eb0ed986ce5b84c013996163c8817e77cb81
CarlosOjeda25 <duvan.duvanojeda@gmail.com>
2025-10-07 19:09:45 -0500

    feat: Implement product management with REST API and HTML frontend

--- Inventario_productos/productos/urls.py
+++ Inventario_productos/productos/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path,include
-from .views import ProductoListView, ProductoDeleteView
+from .views import ProductoListAPIView, ProductoDeleteAPIView, ProductoListView, ProductoDeleteView, DemoView
 
 urlpatterns = [
-    path('productos/', ProductoListView.as_view(), name='producto-list'),
-    path('productos/<int:pk>/delete', ProductoDeleteView.as_view(), name='producto-delete'),
+    path('api/productos/', ProductoListAPIView.as_view(), name='produto-list-api'),
+    path('api/productos/<int:pk>/delete', ProductoDeleteAPIView.as_view(), name='producto-delete-api'),
+
+    #HTML Frontend
+    path('productos/',ProductoListView.as_view(), name='producto-list-html'),
+    path('productos/<int:pk>/delete/', ProductoDeleteView.as_view(), name='producto-delete-html'),
+    path('demo/', DemoView.as_view(), name='demo'),
+    path('',ProductoListView.as_view(), name='home'),
 ]
--- Inventario_productos/productos/views.py
+++ Inventario_productos/productos/views.py
@@ -1,14 +1,95 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
+from django.contrib import messages
+from django.views.generic import ListView, DeleteView
+from django.urls import reverse_lazy
+from json import JsonResponse
+
 from.models import Producto
 from .serializers import ProductoSerializer
 from rest_framework import generics
 
+
 # Listar Productos
-class ProductoListView(generics.ListAPIView):
+class ProductoListAPIView(generics.ListAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
 #Eliminar Producto
-class ProductoDeleteView(generics.DestroyAPIView):
+class ProductoDeleteAPIView(generics.DestroyAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+
+#Html views para frontend
+class ProductoListView(ListView):
+    model = Producto
+    template_name = 'productos/producto_list-html'
+    context_object_name = 'productos'
+    ordeingring = ['-creado']
+    
+class DemoView(ListView):
+    model = Producto
+    template_name = 'productos/demo.html'
+    context_object_name = 'productos'
+
+
+class ProductoDeleteView(DeleteView):
+    model = Producto
+    template_name = 'productos/producto_confirm_delete-html'
+    success_url = reverse_lazy('producto-list.html')
+    context_object_name = 'producto'
+
+    def delete(self, request, *args, **kwargs):
+        producto = self.get_object()
+        messages.success(request, f'El Producto "{producto.nombre}" eliminado exitosamente.')
+        return super().delete(request, *args, **kwargs)
+    
+# vista para manejar ajax
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ProductoAjaxView(generics.GenericAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+    def post(self, request, *args, **kwargs):
+        """Crear nuevo producto via AJAX"""
+        
+        try:
+            data ={
+                'nombre': request.POST.get('nombre'),
+                'descripcion': request.POST.get('descripcion',''),
+                'precio': request.POST.get('precio'),
+            }
+
+            producto =  property.objects.create(**data)
+            return JsonResponse({
+                'id': producto.id,
+                'nombre': producto.nombre,
+                'descripcion': producto.descripcion,
+                'precio': str(producto.precio),
+                'creado': producto.creado.strftime('%Y-%m-%d %H:%M:%S')
+            })
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+        
+
+    def put(self, request, *args, **kwargs):
+        """Crear nuevo producto via AJAX"""
+        
+        try:
+            data ={
+                'nombre': request.POST.get('nombre'),
+                'descripcion': request.POST.get('descripcion',''),
+                'precio': request.POST.get('precio'),
+            }
+
+            producto =  property.objects.create(**data)
+            return JsonResponse({
+                'id': producto.id,
+                'nombre': producto.nombre,
+                'descripcion': producto.descripcion,
+                'precio': str(producto.precio),
+                'creado': producto.creado.strftime('%Y-%m-%d %H:%M:%S')
+            })
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
